bc21a3fb7565f169557c3b42db79ed7b334daff1
Ruiqi <crqricky24@gmail.com>
2022-04-08 10:24:47 -0500

    Revert "Change .gitignore to include output csv"
    This reverts commit 2220ebbe19c53686766175945cb0045813ab326e.

--- code/spatial/multivariate_projections_within-task.R
+++ code/spatial/multivariate_projections_within-task.R
@@ -33,7 +33,7 @@ n_resamples <- 100
 ## execute ----
 
 atlas <- get(atlas_nm)
-rois <- unique(atlas$key[[roi_col]])
+rois <- unique(atlas$key[[roi_col]])[1:10]
 waves <- waves[do_waves]
 n_classes <- length(classes)
 
