ce16cbe027b62c15bc57ab1aeefcc84c44059f8a
mike freund <m.freund@wustl.edu>
2022-09-07 13:27:31 -0500

    Add more scatterplots

--- code/inferential/response_comparison_model.r
+++ code/inferential/response_comparison_model.r
@@ -5,7 +5,7 @@ library(rmarkdown)
 library(mfutils)
 
 mv <- "rda"
-model <- c("fixed", "no_lscov", "no_lscov_symm", "no_lscov", "full")
+model <- c("fixed_sigma", "no_lscov", "no_lscov_symm", "no_lscov", "full")
 pars <- data.frame(mv = mv, model = model, stringsAsFactors = FALSE)
 
 for (i in seq_len(nrow(pars))) {
